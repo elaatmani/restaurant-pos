2a97915b2337938239ff91b44d8fc60f199994b9
ya9 <devyassine47@gmail.com>
2023-11-05 02:22:26 +0100

    updated config.js

--- src/config/app.js
+++ src/config/app.js
@@ -1,6 +1,6 @@
 
-export const frontendUrl = 'http://localhost:8080/'
-// export const frontendUrl = 'http://192.168.147.46:8080/'
+// export const frontendUrl = 'http://localhost:8080/'
+export const frontendUrl = 'https://pos.elaatmani.com/'
 export const backendUrl = 'http://localhost:8000/'
 
 export const currency = "DH";
