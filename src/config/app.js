a33ec7a821cff3ac4c35e309ddb83e92308edc8f
ya9 <devyassine47@gmail.com>
2023-12-09 22:30:58 +0100

    added orders to dashboard

--- src/config/app.js
+++ src/config/app.js
@@ -1,8 +1,8 @@
 
-export const frontendUrl = 'http://localhost:8080/'
-export const backendUrl = 'http://localhost:8000/'
-// export const frontendUrl = 'https://pos.elaatmani.com/'
-// export const backendUrl = 'https://api-pos.elaatmani.com/'
+// export const frontendUrl = 'http://localhost:8080/'
+// export const backendUrl = 'http://localhost:8000/'
+export const frontendUrl = 'https://pos.elaatmani.com/'
+export const backendUrl = 'https://api-pos.elaatmani.com/'
 
 export const currency = "DH";
 export const prefix = "pos_";
